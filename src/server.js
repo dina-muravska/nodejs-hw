711d9538685fe2f52b4a0d267989023f3983e0c0
Dina Muravska <dina.muravska.25@gmail.com>
2026-05-06 22:04:53 +0300

    add middleware

--- src/server.js
+++ src/server.js
@@ -1,36 +1,29 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
-import pino from 'pino-http';
 import helmet from 'helmet';
-// import { connectMongoDB } from './db/connectMongoDB.js';
-// import { errorHandler } from './middleware/errorHandler.js';
-// import { notFoundHandler } from './middleware/notFoundHandler.js';
-// import { logger } from './middleware/logger.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { errorHandler } from './middleware/errorHundler.js';
+import { notFoundHandler } from './middleware/notFoundHundler.js';
+import { logger } from './middleware/logger.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 // app.use(logger);
 app.use(express.json({}));
+
 app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+
+app.use(logger);
+
 app.use(helmet());
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
